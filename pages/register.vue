ae70ac7f8b5784dd6dd7587a7764fe0ac2926b68
aAxiong <879628926@qq.com>
2020-06-17 09:09:43 +0800

    登陆

--- pages/register.vue
+++ pages/register.vue
@@ -58,6 +58,7 @@
 </template>
 
 <script>
+import CryptoJs from "crypto-js";
 export default {
   layout: "blank",
 
@@ -120,8 +121,58 @@ export default {
     };
   },
   methods: {
-    sendMsg: function() {},
-    register: function() {}
+    sendMsg: function() {
+      const self = this;
+      let namePass;
+      let emailPass;
+      if (self.timerid) {
+        return false;
+      }
+      this.$refs["ruleForm"].validateField("name", valid => {
+        namePass = valid;
+      });
+      self.statusMsg = "";
+      if (namePass) {
+        return false;
+      }
+      this.$refs["ruleForm"].validateField("email", valid => {
+        emailPass = valid;
+      });
+      if (!namePass && !emailPass) {
+        self.$axios
+          .post("/users/verify", {
+            username: encodeURIComponent(self.ruleForm.name),
+            email: self.ruleForm.email
+          })
+          .then(({ status, data }) => {
+            if ((status === 200 && data && data, cpde === 0)) {
+              let count = 60;
+              self.statusMsg = `验证码已发送,剩余${count--}秒`;
+              self.timerid = setInterval(function() {
+                self.statusMsg = `验证码已发送,剩余${count--}秒`;
+                if (count === 0) {
+                  clearInterval(self.timerid);
+                }
+              }, 1000);
+            } else {
+              self.statusMsg = data.msg;
+            }
+          });
+      }
+    },
+    register: function() {
+      let self = this;
+      this.$refs["ruleForm"].validate(valid => {
+        if (valid) {
+          self.$axios.post("/user/signup", {
+            username: window.encodeURI(self.ruleForm.name),
+            password: CryptoJs.MD5(self.ruleForm.pwd).toString(),
+            email: self.ruleForm.email,
+            code: self.ruleForm.code
+          });
+        }
+      });
+    }
   }
 };
 </script>
--- server/interface/user.js
+++ server/interface/user.js
@@ -7,38 +7,41 @@ import Email from '../dbs/config'
 import axios from './utils/axios'
 
 let router = new Router({
-  preifx: '/users'
+  prefix: '/users'
 })
 
 let Store = new Redis().client
-router.post('/signuip', async (ctx) => {
+
+router.post('/signup', async (ctx) => {
   const {
     username,
     password,
     email,
     code
   } = ctx.request.body;
+
   if (code) {
-    const savaCode = await Store.hget(`nodemail:${username}`, 'code');
-    const savaExpire = await Store.hgit(`nodemail:${username}`, 'expire')
-    if (code == savaCode) {
-      if (new Date().getTime() - savaExpire > 0) {
-        ctx.body = {
-          code: -1,
-          msg: '验证码已过期，请重新尝试'
-        }
-        return false
-      } else {
-        ctx.body = {
-          code: -1,
-          msg: '请填写正确的验证码'
-        }
-      }
-    } else {
+    // const saveCode = await Store.hget(`nodemail:${username}`, 'code')
+    const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
+    // if (code === saveCode) {
+    if (new Date().getTime() - saveExpire > 0) {
       ctx.body = {
         code: -1,
-        mag: '请填写验证码'
+        msg: '验证码已过期，请重新尝试'
       }
+      return false
+    }
+    // } 
+    // else {
+    //   ctx.body = {
+    //     code: -1,
+    //     msg: '请填写正确的验证码'
+    //   }
+    // }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: '请填写验证码'
     }
   }
   let user = await User.find({
@@ -57,11 +60,11 @@ router.post('/signuip', async (ctx) => {
     email
   })
   if (nuser) {
-    let res = await axios.post('/user/signin', {
+    let res = await axios.post('/users/signin', {
       username,
       password
     })
-    if (res.data && res.data.code == 0) {
+    if (res.data && res.data.code === 0) {
       ctx.body = {
         code: 0,
         msg: '注册成功',
@@ -70,13 +73,13 @@ router.post('/signuip', async (ctx) => {
     } else {
       ctx.body = {
         code: -1,
-        msg: "error"
+        msg: 'error'
       }
     }
   } else {
     ctx.body = {
-      code : -1,
-      msg: "注册失败"
+      code: -1,
+      msg: '注册失败'
     }
   }
 })
@@ -92,7 +95,8 @@ router.post('/signin', async (ctx, next) => {
       if (user) {
         ctx.body = {
           code: 0,
-          msg: '登录成功'
+          msg: '登录成功',
+          user
         }
         return ctx.login(user)
       } else {
@@ -111,14 +115,12 @@ router.post('/verify', async (ctx, next) => {
   if (saveExpire && new Date().getTime() - saveExpire < 0) {
     ctx.body = {
       code: -1,
-      msg: "验证请求过于频繁,1分钟内1次"
+      msg: '验证请求过于频繁，1分钟内1次'
     }
     return false
   }
   let transporter = nodeMailer.createTransport({
-    host: Email.smtp.host,
-    port: 587,
-    secure: false,
+    service: 'qq',
     auth: {
       user: Email.smtp.user,
       pass: Email.smtp.pass
@@ -131,22 +133,21 @@ router.post('/verify', async (ctx, next) => {
     user: ctx.request.body.username
   }
   let mailOptions = {
-    from: `"认证邮件"<${Email.smtp.user}>`,
+    from: `"认证邮件" <${Email.smtp.user}>`,
     to: ko.email,
-    subject: '注册码',
-    html: `您在注册，您的邀请码是${ko.code}`
+    subject: '《慕课网高仿美团网全栈实战》注册码',
+    html: `您在《慕课网高仿美团网全栈实战》课程中注册，您的邀请码是${ko.code}`
   }
-
   await transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      return console.log('error')
+      return console.log(error)
     } else {
-      Store.hmset(`nodemail${ko.user}`, 'code', ko.code, 'expire', ko.expire, 'email', ko.email)
+      Store.hmset(`nodemail:${ko.user}`, 'code', ko.code, 'expire', ko.expire, 'email', ko.email)
     }
   })
   ctx.body = {
     code: 0,
-    msg: '验证码已发送 有效期1分钟'
+    msg: '验证码已发送，可能会有延时，有效期1分钟'
   }
 })
 
@@ -164,12 +165,10 @@ router.get('/exit', async (ctx, next) => {
 })
 
 router.get('/getUser', async (ctx) => {
-  if (!ctx.isAuthenticated()) {
+  if (ctx.isAuthenticated()) {
     const {
       username,
-      password,
-      email,
-      code
+      email
     } = ctx.session.passport.user
     ctx.body = {
       user: username,
@@ -182,3 +181,5 @@ router.get('/getUser', async (ctx) => {
     }
   }
 })
+
+export default router
